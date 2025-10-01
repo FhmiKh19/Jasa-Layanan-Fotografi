3ed4650d55b28a291741b20b9c78561122046a0b
FahmiKh <fahmikhairawan34@gmail.com>
2025-10-01 14:52:52 +0700

    Menambahkan Passing Data ke View

--- resources/views/home-fotografi.blade.php
+++ resources/views/home-fotografi.blade.php
@@ -186,7 +186,7 @@
     </div>
   </footer>
 
-  <!-- JS -->
+  {{--Java Script --}}
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
   <script>
